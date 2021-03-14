065957b9a800fbb7aac48def39d1f28ffe4e9550
Take-Some-Bytes <horton0712@gmail.com>
2021-03-13 21:08:03 -0800

    Updated logging and loggers

--- bin/bin.js
+++ bin/bin.js
@@ -10,7 +10,25 @@ process.title = `cw-server-${process.env.INSTANCE_NUM}`
 
 ;(async () => {
   const server = await GameServer.create(process.argv[2])
-  server.start()
+  const serverLogger = server.loggers.get('Server-logger')
+
+  server.start().then(serverStarted => {
+    if (!serverStarted) {
+      serverLogger.warning('Server is already in STARTING or STARTED state!')
+    } else {
+      serverLogger.info(
+        `Server started at http://${server.config.get('HOST')}:${server.config.get('PORT')}.`
+      )
+    }
+  }).catch(err => {
+    if (err.code === 'ENOTINITIALIZED') {
+      serverLogger.alert('Server did not initialize before starting!')
+      serverLogger.alert('There is likely a fatal bug in the code.')
+    } else {
+      serverLogger.crit('Error while starting server! Error is:')
+      serverLogger.crit(err.stack)
+    }
+  })
 
   process.on('SIGINT', shutdown.handleShutdown.bind(null, server))
   process.on('SIGTERM', shutdown.handleShutdown.bind(null, server))
