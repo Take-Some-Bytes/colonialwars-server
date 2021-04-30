6a5ee1f42c6682c3224a00290682c475fedff5ee
Take-Some-Bytes <horton0712@gmail.com>
2021-04-29 21:21:13 -0700

    Replaced references to GameServer class with CWServer

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
  */
 
 /**
- * The Colonial Wars GameServer class, to handle backend game logic.
+ * The Colonial Wars CWServer class, to handle backend game logic.
  */
-module.exports = exports = require('./lib/game-server')
+module.exports = exports = require('./lib/cw-server')
