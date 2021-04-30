d9dc49a3958bce8d2936ef59e4d39fb5f0bcabfd
Take-Some-Bytes <horton0712@gmail.com>
2021-04-29 21:09:21 -0700

    Added a game authorization route

--- spec/controllers.spec.js
+++ spec/controllers.spec.js
@@ -7,7 +7,9 @@
  */
 
 const http = require('http')
+const crypto = require('crypto')
 const Router = require('router')
+const qs = require('querystring')
 
 const Controllers = require('../lib/controllers/controllers')
 const ServerUtils = require('../lib/utils/server-utils')
@@ -15,6 +17,8 @@ const ServerUtils = require('../lib/utils/server-utils')
 const fetch = require('./helpers/fetch')
 const MockServerUtils = require('./mocks/internal/mock-server-utils')
 const MockLoggers = require('./mocks/internal/mock-loggers')
+const MockRequest = require('./mocks/external/mock-http-request')
+const MockResponse = require('./mocks/external/mock-http-response')
 
 describe('The Controllers class,', () => {
   const serverUtils = new ServerUtils({
@@ -168,5 +172,122 @@ describe('The controllers.registerTestRoute() method,', () => {
     expect(serverRes.meta.statusCode).toBe(404)
     expect(serverRes.body).toBeInstanceOf(Buffer)
     expect(serverRes.body.toString('utf-8')).toBe('Not Found')
+  })
+})
+
+describe('The /game-auth route,', () => {
+  const mockServerUtils = new MockServerUtils()
+  const mockAuthDB = {
+    /**
+     * @type {Map<string, string>}
+     */
+    _store: new Map(),
+    get: async function (key) {
+      return this._store.get(key)
+    },
+    set: async function (key, val) {
+      this._store.set(key, val)
+    },
+    has: async function (key) {
+      return this._store.has(key)
+    },
+    del: async function (key) {
+      return this._store.delete(key)
+    }
+  }
+  const ctlrs = new Controllers({
+    gameAuthSecret: 'game-auth-secret',
+    authDB: mockAuthDB,
+    serverUtils: mockServerUtils
+  })
+  const router = Router()
+
+  beforeAll(() => {
+    router.use((req, res, next) => {
+      const url = new URL(req.url, 'http://example.com')
+      req.query = url.searchParams
+      next()
+    })
+    router.use('/game-auth', ctlrs.gameAuthRouter())
+  })
+
+  describe('the /get endpoint,', () => {
+    it('should return an error if no query is given', done => {
+      const mockReq = new MockRequest({
+        url: '/game-auth/get',
+        method: 'GET'
+      })
+
+      router(mockReq, null, () => {
+        done.fail(new Error('Route not handled'))
+      })
+
+      setTimeout(() => {
+        expect(mockServerUtils.numErrorsSent).toBe(1)
+        expect(mockServerUtils.errors['Error-1'].error.httpOpts.status).toBe(400)
+        done()
+      }, 100)
+    })
+
+    it('should return an error if query does not include required fields', done => {
+      const mockReq = new MockRequest({
+        url: `/game-auth/get?player=${encodeURIComponent('[object Object]')}`,
+        method: 'GET'
+      })
+
+      router(mockReq, null, () => {
+        done.fail(new Error('Route not handled'))
+      })
+
+      setTimeout(() => {
+        expect(mockServerUtils.numErrorsSent).toBe(2)
+        expect(mockServerUtils.errors['Error-2'].error.httpOpts.status).toBe(400)
+        done()
+      }, 100)
+    })
+
+    it('should return the SHA-256 HMAC of the passed-in fields', done => {
+      const data = {
+        playerName: 'NOPE',
+        playerTeam: 'Franch',
+        playerGame: '7H3_B357_G4M3_0N_7H15_53RV3R'
+      }
+      const mockReq = new MockRequest({
+        url: `/game-auth/get?${qs.stringify(
+          Object.fromEntries(
+            Object.entries(data).map(entry => [entry[0].toLowerCase(), entry[1]])
+          ))}`,
+        method: 'GET'
+      })
+      const mockRes = new MockResponse()
+      const unexpectedHmac = crypto.createHmac('sha256', 'game-auth-secret')
+        .update(JSON.stringify(data))
+        .digest()
+        .toString('hex')
+
+      router(mockReq, mockRes, () => {
+        done.fail(new Error('Route not handled'))
+      })
+
+      setTimeout(async () => {
+        expect(mockServerUtils.numErrorsSent).toBe(2)
+        expect(mockServerUtils.errors['Error-3']).toBe(undefined)
+        expect(mockRes.headers['Content-Type']).toBe('application/json')
+        expect(mockRes.responseContent).toBeInstanceOf(Buffer)
+        expect(JSON.parse(mockRes.responseContent.toString('utf-8'))).not.toEqual({
+          status: 'ok',
+          data: {
+            auth: unexpectedHmac
+          }
+        })
+        expect(JSON.parse(mockRes.responseContent.toString('utf-8'))).toEqual({
+          status: 'ok',
+          data: {
+            auth: JSON.parse(await mockAuthDB.get('NOPE')).auth
+          }
+        })
+        done()
+      }, 100)
+    })
   })
 })
