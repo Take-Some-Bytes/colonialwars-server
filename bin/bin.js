ba40bcaf51ad2b39887e28ebdf11f9775fa307a7
Take-Some-Bytes <horton0712@gmail.com>
2022-04-23 16:34:18 -0700

    Use ServerConfig class for configuration
    - Removed ability to pass in JSON config file
    - Added dotenv for loading .env files

--- bin/bin.js
+++ bin/bin.js
@@ -9,7 +9,7 @@ const shutdown = require('./shutdown')
 process.title = `cw-server-${process.env.INSTANCE_NUM}`
 
 ;(async () => {
-  const server = await CWServer.create(process.argv[2])
+  const server = await CWServer.create()
   const serverLogger = server.loggers.get('Server-logger')
 
   server.start().then(serverStarted => {
@@ -17,7 +17,7 @@ process.title = `cw-server-${process.env.INSTANCE_NUM}`
       serverLogger.warning('Server is already in STARTING or STARTED state!')
     } else {
       serverLogger.info(
-        `Server started at http://${server.config.get('HOST')}:${server.config.get('PORT')}.`
+        `Server started at http://${server.config.HOST}:${server.config.PORT}.`
       )
     }
   }).catch(err => {
