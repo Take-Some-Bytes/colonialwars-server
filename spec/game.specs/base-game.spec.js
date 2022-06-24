04af5de607a8b8f015667c64f9ac5d31ac9a850f
Take-Some-Bytes <horton0712@gmail.com>
2022-06-24 14:37:47 -0700

    Update BaseGame specs to be independent of each other

--- spec/game.specs/base-game.spec.js
+++ spec/game.specs/base-game.spec.js
@@ -11,99 +11,67 @@ const nanoid = require('nanoid')
 const Vector2D = require('../../lib/game/physics/vector-2d')
 const { BaseGame } = require('../../lib/game/game-modes')
 
-describe('The BaseGame class,', () => {
-  /**
-   * @type {Array<{ meta: Record<'name'|'team', string>, id: string }>}
-   */
-  const players = []
-  let baseGame = null
+const TESTING_PLAYERS = [
+  { meta: { name: 'GENERAL LOUDSPEAKER', team: 'one' }, id: nanoid.nanoid() },
+  { meta: { name: 'THISISTHEPOLICE', team: 'two' }, id: nanoid.nanoid() },
+  { meta: { name: 'socialsecurity', team: 'one' }, id: nanoid.nanoid() },
+  { meta: { name: 'FBIOPENUP', team: 'two' }, id: nanoid.nanoid() }
+]
 
-  it('should construct without error', () => {
-    let err = null
-    try {
-      baseGame = new BaseGame({
-        id: 'V3RY-UN1QU3-1D',
-        mapConfig: {
-          mapName: 'Base Game 1',
-          mode: 'Teams',
-          maxPlayers: 4,
-          description: 'Testing this game.',
-          worldLimits: { x: 200, y: 200 },
-          teams: [
-            {
-              name: 'one',
-              spawnPosition: new Vector2D(0, 0),
-              description: 'Team one.',
-              maxPlayers: 2
-            },
-            {
-              name: 'two',
-              spawnPosition: new Vector2D(200, 200),
-              description: 'Team two.',
-              maxPlayers: 2
-            }
-          ],
-          tileType: 'grass',
-          player: {
-            speed: 0.4
-          }
+/**
+ * Creates a BaseGame instance for testing.
+ * @returns {BaseGame}
+ */
+function createBaseGame () {
+  const game = new BaseGame({
+    id: 'V3RY-UN1QU3-1D',
+    mapConfig: {
+      mapName: 'Base Game 1',
+      mode: 'Teams',
+      maxPlayers: 4,
+      description: 'Testing this game.',
+      worldLimits: { x: 200, y: 200 },
+      teams: [
+        {
+          name: 'one',
+          spawnPosition: new Vector2D(0, 0),
+          description: 'Team one.',
+          maxPlayers: 2
+        },
+        {
+          name: 'two',
+          spawnPosition: new Vector2D(200, 200),
+          description: 'Team two.',
+          maxPlayers: 2
         }
-      })
-      baseGame.init()
-    } catch (ex) {
-      err = ex
+      ],
+      tileType: 'grass',
+      player: {
+        speed: 0.4
+      }
     }
-
-    expect(err).toBe(null)
-    expect(baseGame).toBeInstanceOf(BaseGame)
   })
+  game.init()
 
+  return game
+}
+
+describe('The BaseGame class,', () => {
   describe('The .addPlayer() method,', () => {
     it('should be able to add new players when space is available', () => {
-      let err = null
-      const newPlayers = [
-        {
-          meta: {
-            name: 'GENERAL LOUDSPEAKER',
-            team: 'one'
-          },
-          id: nanoid.nanoid()
-        },
-        {
-          meta: {
-            name: 'THISISTHEPOLICE',
-            team: 'two'
-          },
-          id: nanoid.nanoid()
-        },
-        {
-          meta: {
-            name: 'socialsecurity',
-            team: 'one'
-          },
-          id: nanoid.nanoid()
-        }
-      ]
+      const baseGame = createBaseGame()
 
-      try {
-        newPlayers.forEach(player => {
-          if (baseGame instanceof BaseGame) {
-            baseGame.addPlayer(player.id, player.meta)
-            players.push(player)
-          }
-        })
-      } catch (ex) {
-        err = ex
-      }
+      TESTING_PLAYERS.slice(0, 3).forEach(player => {
+        baseGame.addPlayer(player.id, player.meta)
+      })
 
-      expect(err).toBe(null)
       expect(baseGame.full).toBe(false)
       expect(baseGame.players.size).toBe(3)
       expect(baseGame.currentPlayers).toBe(3)
     })
 
     it('should not accept players when .closed property is true', () => {
-      let err = null
+      const baseGame = createBaseGame()
       const player = {
         meta: {
           name: 'Let me in please!',
@@ -112,55 +80,58 @@ describe('The BaseGame class,', () => {
         id: nanoid.nanoid()
       }
 
-      try {
-        if (baseGame instanceof BaseGame) {
-          baseGame.closed = true
-          baseGame.addPlayer(player.id, player.meta)
-          players.push(player)
-        } else {
-          throw new TypeError()
-        }
-      } catch (ex) {
-        err = ex
+      TESTING_PLAYERS.slice(0, 3).forEach(player => {
+        baseGame.addPlayer(player.id, player.meta)
+      })
+
+      baseGame.closed = true
+
+      const func = () => {
+        baseGame.addPlayer(player.id, player.meta)
       }
 
-      expect(err).toBeInstanceOf(RangeError)
-      expect(err.message).toBe('Could not add player. Game is either full or closed.')
+      expect(baseGame.full).toBeFalse()
+      expect(baseGame.closed).toBeTrue()
+      expect(baseGame.players.size).toBe(3)
+      expect(baseGame.currentPlayers).toBe(3)
+      expect(func).toThrowError(RangeError)
+      expect(func).toThrowError('Could not add player. Game is either full or closed.')
     })
 
     it('should not accept players when game is full', () => {
-      const errors = []
-      const newPlayers = [
-        { meta: { name: 'FBIOPENUP', team: 'two' }, id: nanoid.nanoid() },
-        { meta: { name: 'Let me in please!', team: 'two' }, id: nanoid.nanoid() }
-      ]
+      const baseGame = createBaseGame()
+      const player = {
+        meta: {
+          name: 'Let me in please!',
+          team: 'two'
+        },
+        id: nanoid.nanoid()
+      }
 
-      baseGame.closed = false
-      newPlayers.forEach(player => {
-        try {
-          if (baseGame instanceof BaseGame) {
-            baseGame.addPlayer(player.id, player.meta)
-            players.push(player)
-            errors.push(null)
-          } else {
-            throw new TypeError()
-          }
-        } catch (ex) {
-          errors.push(ex)
-        }
+      // We want to add all four testing players this time.
+      TESTING_PLAYERS.forEach(player => {
+        baseGame.addPlayer(player.id, player.meta)
       })
 
-      expect(errors.length).toBe(2)
-      expect(errors[0]).toBe(null)
-      expect(errors[1]).toBeInstanceOf(RangeError)
-      expect(baseGame.full).toBe(true)
+      const func = () => {
+        baseGame.addPlayer(player.id, player.meta)
+      }
+
+      expect(baseGame.full).toBeTrue()
+      expect(baseGame.closed).toBeFalse()
       expect(baseGame.players.size).toBe(4)
       expect(baseGame.currentPlayers).toBe(4)
+      expect(func).toThrowError(RangeError)
+      expect(func).toThrowError('Could not add player. Game is either full or closed.')
     })
   })
 
   it('should be able to serialize the current state for the specified client', () => {
-    const player = players[1]
+    const baseGame = createBaseGame()
+    const player = TESTING_PLAYERS[1]
+
+    baseGame.addPlayer(player.id, player.meta)
+
     const expectedPosition = player.meta.team === 'one'
       ? { x: 0, y: 0 }
       : { x: 200, y: 200 }
@@ -171,38 +142,44 @@ describe('The BaseGame class,', () => {
   })
 
   it('should be able to remove the specified player', () => {
-    const player = players.splice(2, 1)[0]
+    const baseGame = createBaseGame()
 
-    baseGame.removePlayer(player.id)
+    TESTING_PLAYERS.forEach(player => {
+      baseGame.addPlayer(player.id, player.meta)
+    })
 
-    expect(baseGame.full).toBe(false)
+    expect(baseGame.full).toBeTrue()
+    expect(baseGame.players.size).toBe(4)
+    expect(baseGame.currentPlayers).toBe(4)
+
+    const playerToRemove = TESTING_PLAYERS[2]
+
+    baseGame.removePlayer(playerToRemove.id)
+
+    expect(baseGame.full).toBeFalse()
     expect(baseGame.players.size).toBe(3)
     expect(baseGame.currentPlayers).toBe(3)
-
-    expect(baseGame.getPlayerByID(player.id)).toBeFalsy()
+    expect(baseGame.getPlayerByID(playerToRemove.id)).toBeFalsy()
   })
 
   it('should be able to call every player\'s .update() method', () => {
-    /**
-     * @type {Array<jasmine.Spy<import('../../lib/game/player')['prototype']['update']>>}
-     */
     const spies = []
-    if (baseGame instanceof BaseGame) {
-      const players = new Map(baseGame.players.entries())
-      baseGame.players = new Map(Array.from(players.entries()).map(
-        entry => {
-          const spy = jasmine.createSpy('Player update() method spy', entry[1].update)
-          entry[1].update = spy
-          spies.push(spy)
-          return [entry[0], entry[1]]
-        }
-      ))
-      baseGame.update()
-      // Restore original Player implementations.
-      baseGame.players = new Map(players.entries())
-    }
+    const baseGame = createBaseGame()
 
-    expect(spies.length).toBe(3)
+    TESTING_PLAYERS.forEach(player => {
+      baseGame.addPlayer(player.id, player.meta)
+    })
+
+    baseGame.players.forEach(player => {
+      const spy = jasmine.createSpy('Player update() method spy', player.update)
+      player.update = spy
+
+      spies.push(spy)
+    })
+
+    baseGame.update()
+
+    expect(spies).toHaveSize(TESTING_PLAYERS.length)
     spies.forEach(spy => {
       expect(spy).toHaveBeenCalledTimes(1)
       expect(spy.calls.all().length).toBe(1)
@@ -211,6 +188,16 @@ describe('The BaseGame class,', () => {
   })
 
   it('should be able to clear all players', () => {
+    const baseGame = createBaseGame()
+
+    TESTING_PLAYERS.forEach(player => {
+      baseGame.addPlayer(player.id, player.meta)
+    })
+
+    expect(baseGame.full).toBeTrue()
+    expect(baseGame.players.size).toBe(4)
+    expect(baseGame.currentPlayers).toBe(4)
+
     baseGame.clearPlayers()
 
     expect(baseGame.full).toBe(false)
