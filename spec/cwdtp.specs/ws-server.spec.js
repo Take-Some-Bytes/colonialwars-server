c35078240bce681b6def0d0c73816cf045f59824
Take-Some-Bytes <horton0712@gmail.com>
2022-07-26 21:25:25 -0700

    Rewrite WSServer class to work with updated WSConn
    Also, its sensibility was becoming a concern for me.

--- spec/cwdtp.specs/ws-server.spec.js
+++ spec/cwdtp.specs/ws-server.spec.js
@@ -8,15 +8,18 @@
 
 import http from 'http'
 import events from 'events'
+import timers from 'timers'
 
-import WebSocket, { WebSocketServer } from 'ws'
-import WSConn from '../../lib/cwdtp/conn.js'
+import WebSocket from 'ws'
+
+import WSConn, { WSConnState } from '../../lib/cwdtp/conn.js'
 import WSServer from '../../lib/cwdtp/server.js'
+import * as errors from '../../lib/cwdtp/errors.js'
 
 import getPrivateIp from '../helpers/get-private-ip.js'
 
 /**
- * @type {InstanceType<WSServer>}
+ * @type {WSServer}
  */
 let wsServer = null
 const server = http.createServer((_, res) => {
@@ -68,88 +71,65 @@ function stopServer (done) {
   })
 }
 /**
- * Gets a list of "toggles".
+ * Gets a bunch of spies for a WebSocket
  * @param {WebSocket} ws The WebSocket to work with.
  */
-function getToggles (ws) {
-  const toggles = {
-    hadVerifyClientError: false,
-    handshakeTimedout: false,
-    rejectedHandshake: false,
-    rejectionCode: '',
-    verifyClientError: null,
-    connTimedout: false,
-    connected: false,
-    hadError: false,
-    err: null
+function getWsSpies (ws) {
+  const spies = {
+    wsOpenSpy: jasmine.createSpy('wsOpen', () => {}),
+    errorSpy: jasmine.createSpy('error', _err => {})
   }
-  ws.on('error', ex => {
-    toggles.hadError = true
-    toggles.err = ex
-  })
-  ws.on('disconnect', () => {
-    toggles.connected = false
-  })
-  ws.on('close', () => {
-    toggles.connected = false
-  })
-  wsServer.on('rejectedHandshake', (_, code) => {
-    toggles.rejectedHandshake = true
-    toggles.rejectionCode = code
-  })
-  wsServer.on('verifyClientError', ex => {
-    toggles.hadVerifyClientError = true
-    toggles.verifyClientError = ex
-  })
-  wsServer.on('handshakeTimeout', () => {
-    toggles.handshakeTimedout = true
-  })
-  wsServer.on('connectionError', ex => {
-    toggles.hadError = true
-    toggles.err = ex
-  })
-  wsServer.on('connectionTimeout', () => {
-    toggles.connTimedout = true
-  })
-  return toggles
+
+  ws.on('error', spies.errorSpy)
+  ws.on('open', spies.wsOpenSpy)
+
+  return spies
+}
+
+/**
+ * Gets a bunch of spies for WSServer events.
+ */
+function getServerSpies () {
+  const spies = {
+    rejectedHandshakeSpy: jasmine.createSpy('rejectedHandshake', (_ip, _code) => {}),
+    verifyClientErrorSpy: jasmine.createSpy('verifyClientError', _err => {}),
+    handshakeTimeoutSpy: jasmine.createSpy('handshakeTimeout', _ip => {}),
+    connectionErrorSpy: jasmine.createSpy('connectionError', _err => {}),
+    cwdtpConnectionSpy: jasmine.createSpy('cwdtpConnection', () => {})
+  }
+
+  wsServer.on('rejectedHandshake', spies.rejectedHandshakeSpy)
+  wsServer.on('verifyClientError', spies.verifyClientErrorSpy)
+  wsServer.on('handshakeTimeout', spies.handshakeTimeoutSpy)
+  wsServer.on('connectionError', spies.connectionErrorSpy)
+  wsServer.on('connection', spies.cwdtpConnectionSpy)
+
+  return spies
 }
 
 describe('The WSServer class,', () => {
-  it('should construct without error', () => {
-    let err = null
-
-    try {
-      wsServer = new WSServer({
-        maxConnsPerIP: 3,
-        path: '/',
-        heartBeatInterval: 40000,
-        getClientIP (req) {
-          return req.socket.remoteAddress
-        },
-        handleCors (origin) {
-          return origin && origin === 'http://localhost:4000'
-        },
-        verifyClient (req, cb) {
-          cb(null)
-        }
-      })
-    } catch (ex) {
-      err = ex
-    }
-
-    expect(err).toBe(null)
-    expect(wsServer).toBeInstanceOf(WSServer)
-    expect(wsServer.wsServer).toBeInstanceOf(WebSocketServer)
-  })
-
   it('should be able to attach to the specified "HTTP server"', () => {
     const definitelyAHttpServer = new events.EventEmitter()
 
-    if (wsServer instanceof WSServer) {
-      wsServer.attach(definitelyAHttpServer)
-    }
+    const wsServer = new WSServer({})
 
-    expect(definitelyAHttpServer.listeners('upgrade').length).toBe(1)
+    wsServer.attach(definitelyAHttpServer)
+
+    expect(definitelyAHttpServer.listeners('upgrade')).toHaveSize(1)
+  })
+
+  it('should be able to detach from the specified "HTTP server"', () => {
+    const definitelyAHttpServer = new events.EventEmitter()
+
+    const wsServer = new WSServer({})
+
+    wsServer.attach(definitelyAHttpServer)
+
+    expect(definitelyAHttpServer.listeners('upgrade')).toHaveSize(1)
+
+    wsServer.detach(definitelyAHttpServer)
+
+    expect(definitelyAHttpServer.listeners('upgrade')).toHaveSize(0)
   })
 
   describe('when handling new connections,', () => {
@@ -157,88 +137,171 @@ describe('The WSServer class,', () => {
     afterAll(stopServer)
 
     it('should not try to accept connection if path does not match', done => {
-      const toggles = getToggles(new WebSocket('ws://localhost:2583/nomatch'))
+      const spies = getWsSpies(new WebSocket('ws://localhost:2583/nomatch'))
 
       setTimeout(() => {
-        expect(toggles.hadError).toBe(true)
-        expect(toggles.err).toBeInstanceOf(Error)
+        expect(spies.errorSpy).toHaveBeenCalled()
+
+        const err = spies.errorSpy.calls.first().args[0]
+
+        expect(err).toBeInstanceOf(Error)
+
         done()
-      }, 100)
+      }, 50)
     })
 
     it('should not accept connection if subprotocol is not CWDTP', done => {
-      const toggles = getToggles(new WebSocket('ws://localhost:2583/?hi=hello', {
+      const spies = getWsSpies(new WebSocket('ws://localhost:2583/?hi=hello', {
         origin: 'http://localhost:4000',
         localAddress: '127.0.0.1'
       }))
+      const serverSpies = getServerSpies()
 
       setTimeout(() => {
-        expect(toggles.rejectionCode).toBe('EINVALPROTO')
-        expect(toggles.hadVerifyClientError).toBe(false)
-        expect(toggles.handshakeTimedout).toBe(false)
-        expect(toggles.rejectedHandshake).toBe(true)
-        expect(toggles.verifyClientError).toBe(null)
-        expect(toggles.connected).toBe(false)
-        expect(toggles.hadError).toBe(true)
+        expect(spies.errorSpy).toHaveBeenCalled()
+        expect(serverSpies.rejectedHandshakeSpy)
+          .toHaveBeenCalledOnceWith('127.0.0.1', errors.ServerErrorCodes.INVALID_PROTO)
+
         done()
-      }, 100)
+      }, 50)
     })
 
     it('should not accept connection if CORS does not pass', done => {
-      const toggles = getToggles(new WebSocket('ws://localhost:2583/?hi=hello', 'pow.cwdtp', {
+      const spies = getWsSpies(new WebSocket('ws://localhost:2583/?hi=hello', 'pow.cwdtp', {
         // The one is the death of the connection.
         origin: 'http://localhost:4001',
         localAddress: '127.0.0.1'
       }))
+      const serverSpies = getServerSpies()
 
       setTimeout(() => {
-        expect(toggles.rejectedHandshake).toBe(true)
-        expect(toggles.hadError).toBe(true)
-        expect(toggles.err).toBeInstanceOf(Error)
+        expect(spies.errorSpy).toHaveBeenCalled()
+        expect(serverSpies.rejectedHandshakeSpy)
+          .toHaveBeenCalledOnceWith('127.0.0.1', errors.ServerErrorCodes.CORS_FAILED)
+
         done()
-      }, 100)
+      }, 50)
     })
 
     it("should not accept connection if verifyClient doesn't pass", done => {
-      const toggles = getToggles(new WebSocket('ws://localhost:2583/?NOPE=1', 'pow.cwdtp', {
+      const spies = getWsSpies(new WebSocket('ws://localhost:2583/?NOPE=1', 'pow.cwdtp', {
         origin: 'http://localhost:4000',
         localAddress: '127.0.0.1'
       }))
+      const serverSpies = getServerSpies()
 
       setTimeout(() => {
-        expect(toggles.verifyClientError).toBeInstanceOf(Error)
-        expect(toggles.hadVerifyClientError).toBe(true)
-        expect(toggles.rejectedHandshake).toBe(true)
-        expect(toggles.err).toBeInstanceOf(Error)
-        expect(toggles.hadError).toBe(true)
+        expect(spies.errorSpy).toHaveBeenCalled()
+        expect(serverSpies.rejectedHandshakeSpy)
+          .toHaveBeenCalledOnceWith('127.0.0.1', errors.ServerErrorCodes.VERIFY_FAILED)
+        expect(serverSpies.verifyClientErrorSpy)
+          .toHaveBeenCalledOnceWith(new Error('Invalid query!'))
+
         done()
-      }, 100)
+      }, 50)
     })
 
-    it('should not accept connection if CWDTP handshake does not complete', done => {
-      const realTimeout = setTimeout
+    it('should not accept connection if WebSocket handshake fails', done => {
+      const req = http.request({
+        hostname: 'localhost',
+        port: 2583,
+        path: '/?hi=hello',
+        method: 'GET',
+        localAddress: '127.0.0.1'
+      })
+
+      const serverSpies = getServerSpies()
+
+      const errSpy = jasmine.createSpy('error')
+      const upgradeSpy = jasmine.createSpy('upgrade')
+      const responseSpy = jasmine.createSpy('response', _res => {})
+
+      req.on('error', errSpy)
+      req.on('upgrade', upgradeSpy)
+      req.on('response', responseSpy)
+
+      // All we're missing is a Sec-WebSocket-Key header.
+      req.setHeader('Origin', 'http://localhost:4000')
+      req.setHeader('Connection', 'upgrade')
+      req.setHeader('Upgrade', 'websocket')
+      req.setHeader('Sec-WebSocket-Version', '13')
+      req.setHeader('Sec-WebSocket-Protocol', 'pow.cwdtp')
+
+      req.end()
+
+      timers.setTimeout(() => {
+        expect(errSpy).not.toHaveBeenCalled()
+        expect(upgradeSpy).not.toHaveBeenCalled()
+        expect(responseSpy).toHaveBeenCalled()
+
+        expect(serverSpies.rejectedHandshakeSpy)
+          .toHaveBeenCalledOnceWith('127.0.0.1', errors.ServerErrorCodes.WS_HANDSHAKE_FAILED)
+
+        const res = responseSpy.calls.first().args[0]
+
+        expect(res).toBeInstanceOf(http.IncomingMessage)
+        expect(res.statusCode).toBe(400)
+
+        done()
+      }, 50)
+    })
+
+    it('should not accept connection if CWDTP handshake times out', done => {
       jasmine.clock().install()
+
       const ws = new WebSocket('ws://localhost:2583/?hi=hello', 'pow.cwdtp', {
         origin: 'http://localhost:4000',
         localAddress: '127.0.0.1'
       })
-      const toggles = getToggles(ws)
+      const spies = getWsSpies(ws)
+      const serverSpies = getServerSpies()
 
-      realTimeout(() => {
-        jasmine.clock().tick(31000)
-        realTimeout(() => {
-          expect(toggles.verifyClientError).toBe(null)
-          expect(toggles.handshakeTimedout).toBe(true)
-          expect(toggles.hadVerifyClientError).toBe(false)
-          expect(toggles.rejectedHandshake).toBe(false)
-          expect(toggles.err).toBe(null)
-          expect(toggles.hadError).toBe(false)
-          expect(toggles.connected).toBe(false)
-          jasmine.clock().uninstall()
-          // ws.terminate()
-          done()
-        }, 100)
+      timers.setTimeout(() => {
+        jasmine.clock().tick(40000)
+      }, 50)
+      timers.setTimeout(() => {
+        expect(spies.errorSpy).not.toHaveBeenCalled()
+        expect(serverSpies.rejectedHandshakeSpy).not.toHaveBeenCalled()
+
+        expect(spies.wsOpenSpy).toHaveBeenCalled()
+        expect(serverSpies.cwdtpConnectionSpy).not.toHaveBeenCalled()
+
+        expect(serverSpies.handshakeTimeoutSpy).toHaveBeenCalled()
+
+        jasmine.clock().uninstall()
+
+        done()
       }, 100)
+    })
+
+    it('should not accept connection if CWDTP handshake is invalid', done => {
+      const ws = new WebSocket('ws://localhost:2583/?hi=hello', 'pow.cwdtp', {
+        origin: 'http://localhost:4000',
+        localAddress: '127.0.0.1'
+      })
+      const spies = getWsSpies(ws)
+      const serverSpies = getServerSpies()
+
+      ws.on('open', () => ws.send('NOT VALID CWDTP'))
+
+      timers.setTimeout(() => {
+        expect(spies.wsOpenSpy).toHaveBeenCalled()
+
+        expect(spies.errorSpy).not.toHaveBeenCalled()
+        expect(serverSpies.handshakeTimeoutSpy).not.toHaveBeenCalled()
+        expect(serverSpies.rejectedHandshakeSpy).not.toHaveBeenCalled()
+
+        expect(serverSpies.cwdtpConnectionSpy).not.toHaveBeenCalled()
+
+        expect(serverSpies.connectionErrorSpy).toHaveBeenCalled()
+
+        const err = serverSpies.connectionErrorSpy.calls.first().args[0]
+
+        expect(err).toBeInstanceOf(errors.HandshakeError)
+        expect(err.code).toBe(errors.HandshakeErrorCode.INVALID_CWDTP_MSG)
+
+        done()
+      }, 50)
     })
 
     it('should accept connection if everything passes', done => {
@@ -248,43 +311,24 @@ describe('The WSServer class,', () => {
           localAddress: '127.0.0.1'
         }
       })
-      const toggles = getToggles(conn.ws)
+      const serverSpies = getServerSpies()
 
-      setTimeout(() => {
-        expect(conn.connected).toBe(true)
-        expect(conn.ws.readyState === WebSocket.OPEN).toBe(true)
-        expect(toggles.rejectedHandshake).toBe(false)
-        expect(toggles.hadError).toBe(false)
-        expect(toggles.hadVerifyClientError).toBe(false)
-        expect(toggles.handshakeTimedout).toBe(false)
-        expect(toggles.err).toBe(null)
-        expect(toggles.verifyClientError).toBe(null)
+      timers.setTimeout(() => {
+        const spies = getWsSpies(conn._ws)
+
+        expect(conn.state).toBe(WSConnState.OPEN)
+        expect(conn._ws.readyState).toBe(WebSocket.OPEN)
+
+        expect(serverSpies.cwdtpConnectionSpy).toHaveBeenCalled()
+
+        expect(spies.errorSpy).not.toHaveBeenCalled()
+        expect(serverSpies.rejectedHandshakeSpy).not.toHaveBeenCalled()
+        expect(serverSpies.verifyClientErrorSpy).not.toHaveBeenCalled()
+
+        conn.disconnect(1001, 'Exiting test item')
+
         done()
-      }, 1000)
-    })
-
-    it('should not allow too many connections from one IP address', done => {
-      const conn = new WSConn('ws://localhost:2583/?hi=hello', {
-        wsOpts: {
-          origin: 'http://localhost:4000',
-          localAddress: '127.0.0.1'
-        }
-      })
-      const toggles = getToggles(conn.ws)
-      conn.on('error', ex => {
-        toggles.hadError = true
-        toggles.err = ex
-      })
-
-      setTimeout(() => {
-        expect(toggles.rejectionCode).toBe('ECONNLIMIT')
-        expect(toggles.connected).toBe(false)
-        expect(toggles.rejectedHandshake).toBe(true)
-        expect(conn.id).toBe(null)
-        expect(toggles.hadError).toBe(true)
-        expect(toggles.err).toBeInstanceOf(Error)
-        done()
-      }, 100)
+      }, 50)
     })
   })
 
@@ -307,24 +351,41 @@ describe('The WSServer class,', () => {
           }
         })
       ]
-      const togglesArr = conns.map(conn => getToggles(conn))
+
+      const openSpy = jasmine.createSpy('connect', () => {})
+      const connectionTimeoutSpy = jasmine.createSpy('connectionTimeoutSpy', _connId => {})
       conns.forEach(conn => {
+        conn.on('open', openSpy)
         spyOn(conn, 'pong')
       })
 
-      setTimeout(() => {
-        togglesArr.forEach((toggles, i) => {
-          expect(conns[i].connected).toBe(false)
-          expect(toggles.connTimedout).toBe(true)
-        })
+      wsServer.on('connectionTimeout', connectionTimeoutSpy)
+
+      jasmine.clock().install()
+
+      // Force the server to set up the heartbeat mechanism again.
+      wsServer._startHeartbeat()
+
+      timers.setTimeout(() => {
+        expect(openSpy).toHaveBeenCalledTimes(2)
+
+        jasmine.clock().tick(1500)
+      }, 50)
+      timers.setTimeout(() => {
+        jasmine.clock().tick(1500)
+        jasmine.clock().uninstall()
+      }, 100)
+
+      timers.setTimeout(() => {
         conns.forEach(conn => {
+          expect(conn._ws.readyState).toBe(WebSocket.CLOSED)
           expect(conn.pong).toHaveBeenCalled()
         })
-        conns.forEach(conn => {
-          conn.disconnect(false, 1001, 'Exiting test item')
-        })
+
+        expect(connectionTimeoutSpy).toHaveBeenCalledTimes(2)
+
         done()
-      }, 3500)
+      }, 150)
     })
 
     it('should not terminate connections which do send a pong event in the alloted time', done => {
@@ -342,21 +403,44 @@ describe('The WSServer class,', () => {
           }
         })
       ]
-      const togglesArr = conns.map(conn => getToggles(conn))
+
+      const openSpy = jasmine.createSpy('connect', () => {})
+      const connectionTimeoutSpy = jasmine.createSpy('connectionTimeoutSpy', _connId => {})
+
       conns.forEach(conn => {
+        conn.on('open', openSpy)
         spyOn(conn, 'pong').and.callThrough()
       })
 
-      setTimeout(() => {
-        togglesArr.forEach((toggles, i) => {
-          expect(conns[i].connected).toBe(true)
-          expect(toggles.connTimedout).toBe(false)
-        })
+      wsServer.on('connectionTimeout', connectionTimeoutSpy)
+
+      jasmine.clock().install()
+
+      // Force the server to set up the heartbeat mechanism again.
+      wsServer._startHeartbeat()
+
+      timers.setTimeout(() => {
+        expect(openSpy).toHaveBeenCalledTimes(2)
+
+        jasmine.clock().tick(1500)
+      }, 50)
+      timers.setTimeout(() => {
+        jasmine.clock().tick(1500)
+        jasmine.clock().uninstall()
+      }, 100)
+
+      timers.setTimeout(() => {
         conns.forEach(conn => {
+          expect(conn.state).toBe(WSConnState.OPEN)
           expect(conn.pong).toHaveBeenCalled()
+
+          conn.disconnect(1001, 'Exiting test item')
         })
+
+        expect(connectionTimeoutSpy).toHaveBeenCalledTimes(0)
+
         done()
-      }, 3500)
+      }, 150)
     })
   })
 })
